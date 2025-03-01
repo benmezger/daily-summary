2666b6fdbf159d58468611c088ad4201d341d00c
Ben Mezger <me@benmezger.nl>
2025-03-01 22:23:41 +0100

    Escape backticks for Github issues

--- daily/_summary.py
+++ daily/_summary.py
@@ -21,7 +21,7 @@ def write_summary(repository_summaries: dict[str, list[Summary]], file: TextIO) 
     file.write("\n_Engineering_\n")
 
     for repository, summaries in repository_summaries.items():
-        file.write(f"\n`{repository}`\n")
+        file.write(f"\n\\`{repository}\\`\n")
         for summary in summaries:
             url_tag = "PR" if summary.is_pr else "Issue"
             file.write(f"- {summary.summary} [[{url_tag}]({summary.url})]\n")
